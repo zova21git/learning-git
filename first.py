ad3d428df8ce76e12701a0776674c81aaf6d5cd0
zova21_git <zzova2021@gmail.com>
2021-08-30 11:26:43 -0500

    added a code to first.py

--- first.py
+++ first.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import glob
 import pandas as pd
 import numpy as np
@@ -11,3 +12,18 @@ for fn in files:
     s += sum(pf['Chicago'].to_list())
 
 print(s)
+=======
+import glob
+import pandas as pd
+import numpy as np
+
+files = glob.glob("file path")
+
+s = 0
+
+for fn in files:
+    pf = pd.read_csv(fn)
+    s += sum(pf['Chicago'].to_list())
+
+print(s)
+>>>>>>> 88efeba (added a code to first.py)
